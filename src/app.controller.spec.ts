57e7203f73e35710335c6f66cbeb501d3fc74bca
princedraculla <amirtorkashvand56@gmail.com>
2024-06-12 00:52:13 +0330

    second commit: Introduction

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Expense Tracker API services"', () => {
+      expect(appController.getHello()).toBe('Expense Tracker API services');
     });
   });
 });
